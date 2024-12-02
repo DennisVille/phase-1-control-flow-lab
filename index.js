15befc016b647f016025978dacf921dae1bc8e5d
Dennis Mukuria <denismukuria@gmail.com>
2024-12-02 21:53:44 +0300

    add control flow statements

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+let greeting;
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance< 400)
+  {greeting = `This one is on me!`;
+    return greeting;
+  }
+  else if(distance<2000)
+  {greeting = `That will be twenty bucks.`;
+    return greeting;
+  }
+  else if (distance<2500)
+    {greeting =`I will gladly take your thirty bucks.`;
+      return greeting;
+    }
+  else 
+  {greeting=`No can do.`;
+    return greeting;
+  }
+  
 }
-
-function ternaryCheckCity(){
+let city;
+function ternaryCheckCity(city){
   // Write your code here!
+  city=="NYC" ? greeting = `Ok, sounds good.` : greeting = `No go.`;
+  return greeting;
 }
-
-function switchOnCharmFromTip(){
+let tip;
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip)
+  {
+    case "generous":
+      greeting = `Thank you so much.`;
+      return greeting;
+      break;
+    case "not as generous":
+      greeting = "Thank you.";
+      return greeting;
+      break;
+    default:
+      greeting = `Bye.`;
+      return greeting;
+      break;
+  }  
 }
